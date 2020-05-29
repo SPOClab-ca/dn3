2de51c1bfc64f2764f72efa5f42575bd158ee4da
demetres <demetres.kostas@gmail.com>
2020-05-29 13:05:34 -0400

    Added setup.py

---
+++ dn3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DN3',
+    version='0.1',
+    packages=['data', 'metrics', 'trainable', 'transforms', 'configuratron'],
+    url='https://github.com/SPOClab-ca/DN3/',
+    license='GPL-3.0',
+    author='Demetres Kostas',
+    author_email='demetres.kostas@gmail.com',
+    description=''
+)
