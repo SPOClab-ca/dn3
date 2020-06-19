Deep1010Mapping mask made as bool for easier donwstream masking. Bug with non-existent learning rate for standardclassification fixed DN3BaseModel freezes network body and final classifier independently.

dn3/trainable/models.py
@@ -41,7 +41,7 @@ class DN3BaseModel(nn.Module):
     def save(self, filename):
         torch.save(self.state_dict(), filename)
 
-    def freeze_features(self, unfreeze=False):
+    def freeze_features(self, unfreeze=False, freeze_classifier=False):
         """
         In many cases, the features learned by a model in one domain can be applied to another case.
 
@@ -52,10 +52,13 @@ class DN3BaseModel(nn.Module):
         ----------
         unfreeze : bool
                    To unfreeze weights after a previous call to this.
+        freeze_classifier: bool
+                   Commonly, the classifier layer will not be frozen (default). Setting this to `True` will freeze this
+                   layer too.
         """
         for param in self.parameters():
             param.requires_grad = unfreeze
-        if isinstance(self.classifier, nn.Module):
+        if isinstance(self.classifier, nn.Module) and not freeze_classifier:
             for param in self.classifier.parameters():
                 param.requires_grad = True
 

dn3/trainable/processes.py
@@ -247,7 +247,7 @@ def _check_make_dataloader(dataset, **loader_kwargs):
 
 class StandardClassification(BaseProcess):
 
-    def __init__(self, classifier: torch.nn.Module, loss_fn=None, cuda=None, metrics=None, learning_rate=None,
+    def __init__(self, classifier: torch.nn.Module, loss_fn=None, cuda=None, metrics=None, learning_rate=0.01,
                  **kwargs):
         if isinstance(metrics, dict):
             metrics.setdefault('Accuracy', self._simple_accuracy)

dn3/transforms/basic.py
@@ -215,7 +215,7 @@ class MappingDeep1010(BaseTransform):
         x[SCALE_IND, :] = 0 if self.max_scale is None else scale
 
         if self.return_mask:
-            return (x, self.mapping.sum(dim=0))
+            return (x, self.mapping.sum(dim=0).bool())
         else:
             return x
 